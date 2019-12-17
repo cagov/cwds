09f3df064185114f0c420987aff134bdbca49765
Aaron Hans <aaron.hans@alpha.ca.gov>
2019-12-17 10:55:09 -0800

    remove logs

--- components/accordion/index.js
+++ components/accordion/index.js
@@ -1,15 +1,12 @@
 class CWDSAccordion extends HTMLElement {
   connectedCallback() {
-    console.log('hi')
     this.expandTarget = this.querySelector('.card-container');
     this.expandButton = this.querySelector('.card-header');
     this.expandButton.addEventListener('click', this.listen.bind(this))
   }
 
   listen() {
-    console.log('um')
     let cardBodyHeight = this.parentNode.querySelector('.card-body').clientHeight;
-    console.log(cardBodyHeight)
     if(this.expandTarget.clientHeight > 0) {
       this.expandTarget.style.height = '0px';
     } else {
