c9e07ac396872463f0bc5cf63fa2b97401e5e253
Artem Khomishen <artem.khomishen@alpha.ca.gov>
2020-01-27 15:05:02 -0800

    Updating Accordion code

--- site/content/docs/cali/js/accordion/index.js
+++ site/content/docs/cali/js/accordion/index.js
@@ -7,37 +7,18 @@ class CWDSAccordion extends HTMLElement {
 
   listen() {
     let cardBodyHeight = this.parentNode.querySelector('.card-body').clientHeight;
-    if(this.expandTarget.clientHeight > 0) {
+    if(this.style.display == 'none' || this.expandTarget.clientHeight > 0) {
       this.expandTarget.style.height = '0px';
+      this.querySelector('.card-header').classList.remove('accordion-alpha-open');
+      let expando = this.expandTarget;
+      setTimeout(function() {
+        // expando.style.display = "none";
+      }, 300)
     } else {
-     this.expandTarget.style.height = cardBodyHeight+'px';
+      // this.expandTarget.style.display = "block";
+      this.expandTarget.style.height = cardBodyHeight+'px';
+      this.querySelector('.card-header').classList.add('accordion-alpha-open');
     }
   }
 }
 window.customElements.define('cwds-accordion', CWDSAccordion);
-
-
-class CWDSStepList extends HTMLElement {
-  connectedCallback() {
-    this.expandTargets = this.querySelectorAll('li');
-    this.expandTargets.forEach( (item) => {
-      item.addEventListener('click', this.listen)
-    })
-  }
-
-  listen() {
-    let detailsEl = this.querySelector('.details');
-    if(this.classList.contains('list-open')) {
-      this.classList.remove('list-open')
-    } else {
-      this.classList.add('list-open')
-    }
-    if(!detailsEl.style.height || detailsEl.style.height.indexOf('0px') == 0) {
-      detailsEl.style.height = detailsEl.scrollHeight + 'px';
-    } else {
-      detailsEl.style.height = '0px';
-    }
-  }
-}
-window.customElements.define('cwds-step-list', CWDSStepList);
-
